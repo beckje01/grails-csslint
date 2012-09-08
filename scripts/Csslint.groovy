3335f4643bff541e815fb24f022509b32262e32f
Jeff Beck <beckje01@gmail.com>
2012-09-08 08:15:52 -0500

    Fixed up the script to write to a file and do it in one run for all css files.

--- scripts/Csslint.groovy
+++ scripts/Csslint.groovy
@@ -2,67 +2,36 @@ includeTargets << grailsScript("_GrailsInit")
 
 target(csslint: "Run CssLint on the projects CSS files.")
 {
-  // TODO: Implement script here
 
   parseArguments()
 
 
   def options = [:]
-/*
-
-<target name="csslint">
-    <apply executable="java" failonerror="true" parallel="true">
-        <fileset dir="${src.dir}" includes="** /*.css" />
-        <arg line="-jar"/>
-        <arg path="${lib.dir}/js.jar"/>
-        <arg path="${lib.dir}/csslint-rhino.js" />
-
-        <!-- your customized arguments go here -->
-        <arg line="${cssfiles.clean} --warnings=box-model,floats --errors=ids,important"/>
-
-        <srcfile/>
-    </apply>
-</target>
-
- */
-
-//  ant.exec(executable: 'java', failOnError: true){
-//    arg(line: '-jar')
-//    arg(path: "js.jar")
-//    arg(path: 'csslint-rhino.js')
-//  }
-
-  def cssLint = "lib"+File.separator+"csslint-rhino.js"
-
-  def files = ant.fileset(dir: 'web-app/css')
 
 
+  def cssLint = "lib" + File.separator + "csslint-rhino.js"   //TODO test this when plugin is installed.
 
-  files.each{ file ->
-    println "Checking: "+file
-    ant.path(id: "grails.compile.classpath", compileClasspath)
-    ant.java(className:'org.mozilla.javascript.tools.shell.Main',fork:true,classpathref: 'grails.compile.classpath'){
-//      permissions{
-//        grant(class:'java.util.PropertyPermission', name:'read' )
-//      }
-//      permissions()
-//          {    //This will work but stop after processing one file no need to fork
-//            grant(class:'java.security.AllPermission' )
-//            grant(class:'java.lang.RuntimePermission', name:'exitVM')
-//
-//          }
-      arg(line: '-debug')
-      arg(path: cssLint)
+  def files = ant.fileset(dir: 'web-app/css')  //TODO make configurable
 
+  def output = new File("target/csslint.xml") //TODO make configurable
+
+
+  ant.path(id: "grails.compile.classpath", compileClasspath)
+  ant.java(className: 'org.mozilla.javascript.tools.shell.Main', fork: true, classpathref: 'grails.compile.classpath', output: output) {
+    arg(line: '-debug')
+
+    arg(path: cssLint)
+
+    files.each { file ->
       arg(path: file)
-
-      arg(line: '--warnings=box-model,floats')
-      arg(line: '--errors=ids,important')
-      arg(line:  '--format=lint-xml')
     }
-    println "finished"
+    arg(line: '--warnings=box-model,floats') //What should be warnings
+    arg(line: '--errors=ids,important')  //What should be errors
+    arg(line: '--format=lint-xml') //TODO make format switch for the two versions
   }
 
 }
+
+
 
 setDefaultTarget(csslint)
